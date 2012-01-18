58e0cdd94eb6cae48447a0fc2e4d8416a7ab7442
Your Name <fmemon100@yahoo.com>
2012-01-18 17:40:41 -0500

    updated the invoices controllerp92 to remove extra lines

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,2 +1,10 @@
 module InvoicesHelper
+  def display_purchase(invoice)
+    unless invoice.purchase.nil?
+      invoice.purchase.description 
+    else
+      "(no Purchase yet)"
+    end
+  end
+
 end
